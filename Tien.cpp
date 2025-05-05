75144c89e67be3aff190237ac668e3f6493df724
vantruong05 <147252276+vantruong05@users.noreply.github.com>
2025-05-05 23:51:40 +0700

    Update Tien.cpp

--- Tien.cpp
+++ Tien.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 using namespace std;
 
-int tien = 0;
+int tien = 0;  // Biến toàn cục để lưu trữ số tiền
 
+// Hàm nạp tiền
 int getMoney() {
     int nhaptien;
     cout << "Nhập số tiền bạn muốn nạp vào: ";
@@ -17,16 +18,18 @@ int getMoney() {
     }
 
     tien += nhaptien;
-    cout << "Bạn đã nạp thành công " << nhaptien << " đô!\n";
+    cout << "Bạn đã nạp thành công " << nhaptien << " đô!" << endl;
     return tien;
 }
 
+// Hàm in số dư hiện tại
 void printCurrentMoney() {
     cout << "\nSố dư hiện tại của bạn là: " << tien << " đô\n";
 }
 
+// Hàm trả lại tiền
 void returnMoney() {
     cout << "\nSố tiền trả lại là: " << tien << " đô\n";
     cout << "Trả lại tiền thành công!\n";
-    tien = 0;
+    tien = 0;  // Đặt lại số tiền sau khi trả
 }
