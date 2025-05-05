5fd3095e7c6fb50eb7022a2813f6a3cb89e56934
vantruong05 <147252276+vantruong05@users.noreply.github.com>
2025-05-05 23:18:03 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
+#include "Gia.h"
+#include "Tien.h"
+#include "Menu.h"
+#include "Order.h"
+#include <iostream>
+using namespace std;
 
+int main() {
+    int option;
+
+    while (true) {
+        printCurrentMoney();
+        printMenu();
+        cout << "\nNhập lựa chọn của bạn: ";
+        cin >> option;
+
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Lựa chọn không hợp lệ! Vui lòng nhập lại.\n";
+            continue;
+        }
+
+        orderItem(option);
+    }
+
+    return 0;
+}
