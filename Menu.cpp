939e808b5fe8756410030888bcb8ec11f3698d03
vantruong05 <147252276+vantruong05@users.noreply.github.com>
2025-05-05 23:27:09 +0700

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -1,15 +1,15 @@
-#include "Gia.h"
+#include "Menu.h"
 #include <iostream>
 using namespace std;
 
-void printMenu() {
+void printMenu(const vector<Product>& dsSanPham) {
     cout << "\nMENU:\n";
-    cout << "1. Chó - " << gia[0] << " đô\n";
-    cout << "2. Mèo - " << gia[1] << " đô\n";
-    cout << "3. Gà - " << gia[2] << " đô\n";
-    cout << "4. Dê - " << gia[3] << " đô\n";
-    cout << "5. Trả lại tiền\n";
-    cout << "6. Nạp thêm tiền\n";
-    cout << "7. Thoát và tự động trả tiền lại\n";
+    for (size_t i = 0; i < dsSanPham.size(); ++i) {
+        cout << i + 1 << ". " << dsSanPham[i].ten
+             << " - " << dsSanPham[i].gia << " đô"
+             << " (còn " << dsSanPham[i].soLuong << ")\n";
+    }
+    cout << dsSanPham.size() + 1 << ". Trả lại tiền\n";
+    cout << dsSanPham.size() + 2 << ". Nạp thêm tiền\n";
+    cout << dsSanPham.size() + 3 << ". Thoát và tự động trả tiền lại\n";
 }
-
