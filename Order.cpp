d5743204d94db9e6ef73422070f03061a409132c
vantruong05 <147252276+vantruong05@users.noreply.github.com>
2025-05-05 23:17:15 +0700

    Update Order.cpp

--- Order.cpp
+++ Order.cpp
@@ -1,1 +1,29 @@
+#include "Order.h"
+#include "Gia.h"
+#include "Tien.h"
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void orderItem(int option) {
+    if (option >= 1 && option <= 4) {
+        if (tien >= gia[option - 1]) {
+            tien -= gia[option - 1];
+            cout << "\nBạn đã mua thành công!\n";
+            cout << "Số dư còn lại: " << tien << " đô\n";
+        } else {
+            cout << "\nSố dư không đủ! Vui lòng nạp thêm tiền.\n";
+        }
+    } else if (option == 5) {
+        returnMoney();
+    } else if (option == 6) {
+        getMoney();
+    } else if (option == 7) {
+        returnMoney();
+        cout << "Thoát thành công!\n";
+        exit(0);
+    } else {
+        cout << "\nLựa chọn không hợp lệ! Vui lòng chọn lại.\n";
+    }
+}
 
