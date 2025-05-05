1e4f2fb9c7a9806ee421190e23ad8a3604665a34
vantruong05 <147252276+vantruong05@users.noreply.github.com>
2025-05-05 23:48:12 +0700

    Update Product.h

--- Product.h
+++ Product.h
@@ -2,13 +2,14 @@
 #define PRODUCT_H
 
 #include <string>
-using namespace std;
 
 struct Product {
-    string ten;
+    std::string ten;
     int gia;
     int soLuong;
+
+    // Constructor để khởi tạo Product dễ dàng hơn
+    Product(std::string t, int g, int s) : ten(t), gia(g), soLuong(s) {}
 };
 
 #endif
-
