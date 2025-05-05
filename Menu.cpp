be5760b6888301cda8c2d6be7a7448bd379ada85
vantruong05 <147252276+vantruong05@users.noreply.github.com>
2025-05-05 23:16:25 +0700

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -1,1 +1,15 @@
+#include "Gia.h"
+#include <iostream>
+using namespace std;
+
+void printMenu() {
+    cout << "\nMENU:\n";
+    cout << "1. Chó - " << gia[0] << " đô\n";
+    cout << "2. Mèo - " << gia[1] << " đô\n";
+    cout << "3. Gà - " << gia[2] << " đô\n";
+    cout << "4. Dê - " << gia[3] << " đô\n";
+    cout << "5. Trả lại tiền\n";
+    cout << "6. Nạp thêm tiền\n";
+    cout << "7. Thoát và tự động trả tiền lại\n";
+}
 
